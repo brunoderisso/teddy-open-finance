967f0489124b177107407fbd47d6cf1b3b3c5b05
Bruno Derisso <bruno.derisso@gmail.com>
2025-07-04 22:51:29 -0300

    Remoção do dashboard e customer

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,14 +3,6 @@ import { Home } from './components/home/home';
 
 export const appRoutes: Route[] = [
   {
-    path: 'customer',
-    loadChildren: () => import('customer/Routes').then((m) => m.remoteRoutes),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('dashboard/Routes').then((m) => m.remoteRoutes),
-  },
-  {
     path: '',
     component: Home,
   },
